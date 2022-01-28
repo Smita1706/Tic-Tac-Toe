e0525669bf9c1e925d3219d5912739784043e89a
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-28 09:59:45 +0530

    Added : Use case 2 is added in UC2 branch

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -10,7 +10,24 @@ public class TicTacToeGame {
 			board[i] = ' ';
 		}
 	}
+
+	public static void UC2() {
+		char User;
+		char Computer = ' ';
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the letter X or O :");
+		User = s.next().charAt(0);
+		if(User == 'X') 
+			Computer ='O';
+		else if(User == 'O')
+			Computer ='X';
+		else
+			System.out.println("Please enter the letter only X or O");
+		System.out.println("Computers input is " + Computer);
+	}
+
 	public static void main(String [] args){
 		UC1();
+		UC2();
 	}
 }
